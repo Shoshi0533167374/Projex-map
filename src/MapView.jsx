0334e45f683ffeb6a98c202a7404ad9b322c59e3
talmid2 <talmid2@byta.local>
2025-01-01 12:10:58 +0200

    second step

--- src/MapView.jsx
+++ src/MapView.jsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 export default function MapView({ lat, lon }) {
 
     const map = useMap();  //  יוצר אובייקט מפה
-
     useEffect(() => {
         map.setView([lat, lon]);  //משנה את תצוגת המפה לפי המיקום החדש
     }, [lat, lon, map]);  // נעדכן כל פעם שה- לאט או ה- לון משתנים
