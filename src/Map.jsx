462538b8f83c994c05099d1282cb5d43c46371eb
talmid2 <talmid2@byta.local>
2025-01-01 12:05:10 +0200

    first step

---
+++ src/Map.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import MapView from './MapView';
+
+export default function Map({ lat, lon }) {
+    
+    return (
+        <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true} style={{ height: "40vh", width: "50%" }}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[lat, lon]}>
+                <Popup>
+                    המיקום שלך
+                </Popup>
+            </Marker>
+            <MapView lat={lat} lon={lon} />  {/* העברת המיקום לעדכון המפה */}
+        </MapContainer>
+    );
+}
+
+
---
+++ src/MapView.jsx
@@ -0,0 +1,11 @@
+import { useMap } from 'react-leaflet';
+import { useEffect } from 'react';
+
+export default function MapView({ lat, lon }) {
+
+    const map = useMap();  //  יוצר אובייקט מפה
+
+    useEffect(() => {
+        map.setView([lat, lon]);  //משנה את תצוגת המפה לפי המיקום החדש
+    }, [lat, lon, map]);  // נעדכן כל פעם שה- לאט או ה- לון משתנים
+}
