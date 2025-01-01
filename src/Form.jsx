b4b9427129d78676fb538761f81911e0884961a6
talmid2 <talmid2@byta.local>
2025-01-01 14:30:29 +0200

    perfect

--- src/Form.jsx
+++ src/Form.jsx
@@ -52,94 +52,101 @@ export default function Form() {
 
     return (
         <>
-            <form className='form-style add-form' noValidate onSubmit={handleSubmit(save)}>
+            <div className="container-form-max">
+                <form className='form-style add-form' noValidate onSubmit={handleSubmit(save)}>
+                    <div className="title">
+                        <h1> Search office</h1>
+                    </div>
+                    <div>
+                        {errors.UserName && <div className='err-message'> {errors.UserName.message} </div>}
+                        <input type="text" placeholder="User Name"
+                            {...register("UserName", { required: { value: true, message: "name is require" } })} />
+                    </div>
 
-                <div>
-                    {errors.UserName && <div className='err-message'> {errors.UserName.message} </div>}
-                    <input type="text" placeholder="User Name"
-                        {...register("UserName", { required: { value: true, message: "name is require" } })} />
-                </div>
-
-                <div>
-                    {errors.Address && <div className='err-message'> {errors.Address.message} </div>}
-                    <input
-                        type="text"
-                        placeholder="Address"
-                        {...register("Address", {
-                            required: { value: true, message: "Address is required" },
-                            onChange: (e) => changeAddress(e)
-                        })}
-                    />
-
+                    <div>
+                        {errors.Address && <div className='err-message'> {errors.Address.message} </div>}
+                        <input
+                            type="text"
+                            placeholder="Address"
+                            {...register("Address", {
+                                required: { value: true, message: "Address is required" },
+                                onChange: (e) => changeAddress(e)
+                            })}
+                        />
+                    </div>
                     {arrPlaces.length > 0 && (
-                        <ul className="place-list">
-                            {arrPlaces.map((item, index) => (
-                                <li className="place-li" key={index} onClick={() => chooseAddress(item)}>
-                                    {item.placeName}
-                                </li>
-                            ))}
-                        </ul>
+                        <div>
+                            <ul className="place-list">
+                                {arrPlaces.map((item, index) => (
+                                    <li className="place-li" key={index} onClick={() => chooseAddress(item)}>
+                                        {item.placeName}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
                     )}
+
+
+                    <div>
+                        {errors.Phone && <div className='err-message'> {errors.Phone.message} </div>}
+                        <input type="text" placeholder="Phone"
+                            {...register("Phone", {
+                                required: { value: true, message: "phone is require" },
+                                maxLength: { value: 10, message: "the phone must be 9 or 10 digits" },
+                                minLength: { value: 9, message: "the phone must be 9 or 10 digits" }
+                            })} />
+                    </div>
+
+                    <div>
+                        {errors.Email && <div className='err-message'> {errors.Email.message} </div>}
+                        <input type="email" placeholder="Email"
+                            {...register("Email", {
+                                required: { value: true, message: "mail is require" },
+                                pattern: { value: /^\S+@\S+$/i, message: "email is not in correct format" }
+                            })} />
+                    </div>
+
+                    <div>
+                        {errors.NumRooms && <div className='err-message'> {errors.NumRooms.message} </div>}
+                        <input type="number" placeholder="Num Of Rooms"
+                            {...register("NumRooms", {
+                                required: { value: true, message: "num rooms is require" },
+                                min: { value: 1, message: "you must choose at least one room" }
+                            })} />
+                    </div>
+
+                    <div>
+                        {errors.Distance && <div className='err-message'> {errors.Distance.message} </div>}
+                        <input type="number" placeholder="Distance To Move"
+                            {...register("Distance", {
+                                required: { value: true, message: "distance is require" },
+                                min: { value: 0, message: "you cant choose negative distance" }
+                            })} />
+                    </div>
+
+                    <div>
+                        <input type="checkbox"  {...register("NeedsInternet")} />
+                        <label htmlFor="">Needs Internet</label>
+                    </div>
+
+                    <div>
+                        <input type="checkbox"  {...register("NeedsKitchen")} />
+                        <label htmlFor="">Needs Kitchen</label>
+                    </div>
+
+                    <div>
+                        <input type="checkbox"  {...register("NeedsCoffeeMachine")} />
+                        <label htmlFor="">Needs Coffee Machine</label>
+                    </div>
+
+                    <input type="submit" />
+
+                </form>
+                <div className="map">
+                    <Map lat={selectedPlace.lat} lon={selectedPlace.lon} />
                 </div>
-
-                <div>
-                    {errors.Phone && <div className='err-message'> {errors.Phone.message} </div>}
-                    <input type="text" placeholder="Phone"
-                        {...register("Phone", {
-                            required: { value: true, message: "phone is require" },
-                            maxLength: { value: 10, message: "the phone must be 9 or 10 digits" },
-                            minLength: { value: 9, message: "the phone must be 9 or 10 digits" }
-                        })} />
-                </div>
-
-                <div>
-                    {errors.Email && <div className='err-message'> {errors.Email.message} </div>}
-                    <input type="email" placeholder="Email"
-                        {...register("Email", {
-                            required: { value: true, message: "mail is require" },
-                            pattern: { value: /^\S+@\S+$/i, message: "email is not in correct format" }
-                        })} />
-                </div>
-
-                <div>
-                    {errors.NumRooms && <div className='err-message'> {errors.NumRooms.message} </div>}
-                    <input type="number" placeholder="Num Of Rooms"
-                        {...register("NumRooms", {
-                            required: { value: true, message: "num rooms is require" },
-                            min: { value: 1, message: "you must choose at least one room" }
-                        })} />
-                </div>
-
-                <div>
-                    {errors.Distance && <div className='err-message'> {errors.Distance.message} </div>}
-                    <input type="number" placeholder="Distance To Move"
-                        {...register("Distance", {
-                            required: { value: true, message: "distance is require" },
-                            min: { value: 0, message: "you cant choose negative distance" }
-                        })} />
-                </div>
-
-                <div>
-                    <input type="checkbox"  {...register("NeedsInternet")} />
-                    <label htmlFor="">Needs Internet</label>
-                </div>
-
-                <div>
-                    <input type="checkbox"  {...register("NeedsKitchen")} />
-                    <label htmlFor="">Needs Kitchen</label>
-                </div>
-
-                <div>
-                    <input type="checkbox"  {...register("NeedsCoffeeMachine")} />
-                    <label htmlFor="">Needs Coffee Machine</label>
-                </div>
-
-                <input type="submit" />
-
-            </form>
-
-            <Map lat={selectedPlace.lat} lon={selectedPlace.lon} />
-            
+            </div>
         </>
     );
 
--- src/Map.jsx
+++ src/Map.jsx
@@ -6,7 +6,7 @@ import MapView from './MapView';
 export default function Map({ lat, lon }) {
     
     return (
-        <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true} style={{ height: "40vh", width: "50%" }}>
+        <MapContainer center={[lat, lon]} zoom={13} scrollWheelZoom={true} style={{ height: "40vh", width: "100%" }}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
